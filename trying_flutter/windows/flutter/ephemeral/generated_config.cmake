862d8c0fef1a556babebe569099bdad73e562f48
marcelopaivasou <marcelopaivasou@gmail.com>
2025-11-27 18:41:36 -0300

    lab-namer-app testado ok (#2)
    * testado ok
    
    * Merge remote-tracking branch 'origin/main' into lab-namer-app

---
+++ trying_flutter/windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "P:\\app\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\marcelo\\Documents\\dev\\github\\flutter-labs\\trying_flutter" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=P:\\app\\flutter"
+  "PROJECT_DIR=C:\\Users\\marcelo\\Documents\\dev\\github\\flutter-labs\\trying_flutter"
+  "FLUTTER_ROOT=P:\\app\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\marcelo\\Documents\\dev\\github\\flutter-labs\\trying_flutter\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\marcelo\\Documents\\dev\\github\\flutter-labs\\trying_flutter"
+  "FLUTTER_TARGET=C:\\Users\\marcelo\\Documents\\dev\\github\\flutter-labs\\trying_flutter\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mNDBlOTc2YmVkZmY1N2U2OWUxYjNkODlhN2MyYTNjNjE3YTAzZGFkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\marcelo\\Documents\\dev\\github\\flutter-labs\\trying_flutter\\.dart_tool\\package_config.json"
+)
